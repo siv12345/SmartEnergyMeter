2a4d18974d5cc4fcc118908c4b55dcec184eb081
unseemlycoder <56648438+unseemlycoder@users.noreply.github.com>
2020-10-07 01:36:56 +0530

    Update call.py

--- call.py
+++ call.py
@@ -43,11 +43,11 @@ def meter():
         d[day]=truncate(old+data['101']/100,2)
         if month==3 and day==1 and leapYear(year)==True:
                 today=str(truncate(old+data['101']/100-d[29],2))+'  kWh'
-        else if month==3 and day==1 and leapYear(year)==False:
+        elif month==3 and day==1 and leapYear(year)==False:
                 today=str(truncate(old+data['101']/100-d[28],2))+'  kWh'
-        else if month not in [1,5,7,8,10,12] and day==1:
+        elif month not in [1,5,7,8,10,12] and day==1:
                 today=str(truncate(old+data['101']/100-d[31],2))+'  kWh'
-        else if month in [1,5,7,8,10,12] and day==1:
+        elif month in [1,5,7,8,10,12] and day==1:
                 today=str(truncate(old+data['101']/100-d[30],2))+'  kWh'
         else:
                 today=str(truncate(old+data['101']/100-d[day-1],2))+'  kWh'

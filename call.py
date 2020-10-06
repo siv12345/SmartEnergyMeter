8f2c2d658b339200802cb14162e5130d832db454
unseemlycoder <56648438+unseemlycoder@users.noreply.github.com>
2020-10-07 01:12:16 +0530

    Update call.py

--- call.py
+++ call.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python3
 from flask import Flask, render_template
-import datetime
-import tuyapower
+import datetime as dt, pickle, tuyapower
+
 app = Flask(__name__)
 
 def truncate(n, decimals=0):
@@ -9,24 +10,37 @@ def truncate(n, decimals=0):
 
 
 old=3676.4
-data=total=volt=amp=watt=switch=0
+data=total=volt=today=amp=watt=switch=0
 
 @app.route("/")
-def hello():
-        now = datetime.datetime.now()
-        timeString = now.strftime("%Y-%m-%d %H:%M")
-        data=tuyapower.deviceRaw('018065202cf43238c5a8', '192.168.0.86', 'ef2d47140c02257b', '3.3')['dps']
-        total=str(truncate(old+data['101']/100,2))+' kWh'
-        volt=str(truncate(data['20']/10,1))+'  V'
-        amp=str(data['18'])+'\t  mA'
-        watt=str(truncate(data['19']/10,1))+'  W'
+def meter():
+        day=int(dt.datetime.now().strftime("%d"))
+
+        f=open("/home/pi/pythonTuya/day.dat","rb")
+        d=pickle.load(f)
+        f.close()
+
+        data=tuyapower.deviceRaw('id', '10.10.10.10', 'key', '3.3')['dps']
+        total=str(truncate(old+data['101']/100,2))+'  kWh'
+        volt=str(truncate(data['20']/10,1))+'      V'
+        amp=str(data['18'])+'      mA'
+        watt=str(truncate(data['19']/10,1))+'     W'
         switch='On' if data['1']==True else 'Off'
+
+        d[day]=truncate(old+data['101']/100,2)
+        today=str(truncate(old+data['101']/100-d[day-1],2))+'  kWh'
+
+        f=open("day.dat","wb")
+        pickle.dump(d,f)
+        f.close()
+
         templateData = {
         'volt' : volt,
         'amp' : amp,
         'watt' : watt,
         'state' : switch,
-        'total' : total
+        'total' : total,
+        'today': today
         }
         return render_template('index.html', **templateData)
 
